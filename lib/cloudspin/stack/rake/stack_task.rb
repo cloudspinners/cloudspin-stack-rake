054812520bb5d3b8d0815494faf0821d028c0bfa
kief <kmorris@thoughtworks.com>
2018-09-27 15:47:23 +0100

    Option to download a stack definition zipfile. Intended for use with github repos.

--- lib/cloudspin/stack/rake/stack_task.rb
+++ lib/cloudspin/stack/rake/stack_task.rb
@@ -5,15 +5,16 @@ module Cloudspin
 
       class StackTask < ::Rake::TaskLib
 
-        attr_reader :instance
         attr_reader :environment
         attr_reader :role
+        attr_reader :definition_folder
         attr_reader :configuration_files
 
         def initialize(
             environment = nil,
             role: 'instance',
-            definition_folder: './src',
+            definition_folder: nil, # Should be deprecated
+            definition_location: nil,
             base_folder: '.',
             configuration_files: nil
         )
@@ -22,13 +23,42 @@ module Cloudspin
           @base_folder = base_folder
           @configuration_files = configuration_files || the_usual_configuration_files
 
-          @instance = Cloudspin::Stack::Instance.from_folder(
-            @configuration_files,
-            definition_folder: definition_folder,
-            base_folder: @base_folder,
-            base_working_folder: "#{@base_folder}/work"
-          )
+          @definition_location = if definition_location
+            definition_location
+          elsif definition_folder
+            puts "'definition_folder': is deprecated for Cloudspin::Stack::Rake::StackTask - use 'definition_location' instead"
+            definition_folder
+          else
+            './src'
+          end
+
+          # @remote_zipfile = remote_zipfile
           define
+        end
+
+        def instance
+          @instance ||= begin
+            local_definition_folder = fetch_definition
+            puts "Will use local stack definition files in #{local_definition_folder}"
+            Cloudspin::Stack::Instance.from_folder(
+              @configuration_files,
+              definition_folder: local_definition_folder,
+              base_folder: @base_folder,
+              base_working_folder: "#{@base_folder}/work"
+            )
+          end
+        end
+
+        def fetch_definition
+          if /^http.*\.zip$/.match @definition_location
+            puts "Downloading stack definition source from a remote zipfile"
+            fetch_definition_zipfile
+          elsif /^[\.\/]/.match @definition_location
+            puts "Using local stack definition source"
+            @definition_location
+          else
+            raise UnsupportedStackDefinitionLocationError, @definition_location
+          end
         end
 
         def the_usual_configuration_files
@@ -60,36 +90,86 @@ module Cloudspin
 
         def define
 
-          desc "Create or update stack #{@instance.id}"
+          desc "Create or update stack instance"
           task :up do
-            puts @instance.init_dry
-            puts @instance.up_dry
-            puts @instance.up
+            puts instance.init_dry
+            puts instance.up_dry
+            puts instance.up
           end
 
-          desc "Plan changes to stack #{@instance.id}"
+          desc "Plan changes to stack instance"
           task :plan do
-            puts @instance.init_dry
-            puts @instance.plan_dry
-            puts @instance.plan
+            puts instance.init_dry
+            puts instance.plan_dry
+            puts instance.plan
           end
 
-          desc "Show command line to be run for stack #{@instance.id}"
+          desc "Show command line to be run for stack instance"
           task :dry do
-            puts @instance.init_dry
-            puts @instance.up_dry
+            puts instance.init_dry
+            puts instance.up_dry
           end
 
-          desc "Destroy stack #{@instance.id}"
+          desc "Destroy stack instance"
           task :down do
-            puts @instance.init_dry
-            puts @instance.down_dry
-            puts @instance.down
+            puts instance.init_dry
+            puts instance.down_dry
+            puts instance.down
           end
+        end
 
+        # NOTE: This stuff belongs in a core class, so the CLI and other stuff can use it, too.
+
+        def fetch_definition_zipfile
+          unpack(download_artefact(@definition_location), '.cloudspin/definitions')
+        end
+
+        def download_artefact(artefact_url)
+          download_dir = Dir.mktmpdir(['cloudspin-', '-download'])
+          zipfile = "#{download_dir}/undetermined-spin-stack-artefact.zip"
+          puts "Downloading artefact from #{artefact_url} to #{zipfile}"
+          File.open(zipfile, 'wb') do |saved_file|
+            open(artefact_url, 'rb') do |read_file|
+              saved_file.write(read_file.read)
+            end
+          end
+          zipfile
+        end
+
+        def unpack(zipfile, where_to_put_it)
+          folder_name = path_of_source_in(zipfile)
+          puts "Unzipping #{zipfile} to #{where_to_put_it}"
+          Zip::File.open(zipfile) { |zip_file|
+            zip_file.each { |f|
+              puts "-> #{f.name}"
+              f_path = File.join(where_to_put_it, f.name)
+              FileUtils.mkdir_p(File.dirname(f_path))
+              zip_file.extract(f, f_path) unless File.exist?(f_path)
+            }
+          }
+          puts "Definition unpacked to #{where_to_put_it}/#{folder_name}"
+          "#{where_to_put_it}/#{folder_name}"
+        end
+
+        def path_of_source_in(zipfile_path)
+          File.dirname(path_of_configuration_file_in(zipfile_path))
+        end
+
+        def path_of_configuration_file_in(zipfile_path)
+          zipfile = Zip::File.open(zipfile_path)
+          begin
+            zipfile.entries.select { |entry|
+              /\/stack-definition.yaml$/.match entry.name
+            }.first.name
+          ensure
+            zipfile.close
+          end
         end
 
       end
+
+      class UnsupportedStackDefinitionLocationError < StandardError; end
+
     end
   end
 end
