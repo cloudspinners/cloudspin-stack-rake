f8fae25d5ca665837b865a443efcef3be06171d1
kief <kmorris@thoughtworks.com>
2018-11-05 15:02:33 +0000

    Automatically generate a backend terraform file for projects with remote state configured

--- lib/cloudspin/stack/rake/stack_task.rb
+++ lib/cloudspin/stack/rake/stack_task.rb
@@ -41,13 +41,20 @@ module Cloudspin
           @instance ||= begin
             local_definition_folder = fetch_definition
             puts "Will use local stack definition files in #{local_definition_folder}"
-            Cloudspin::Stack::Instance.from_folder(
+
+            the_instance = Cloudspin::Stack::Instance.from_folder(
               @configuration_files,
               stack_name: stack_name,
               definition_folder: local_definition_folder,
               base_folder: @base_folder,
               base_working_folder: "#{@base_folder}/work"
             )
+
+            if the_instance.configuration.has_remote_state_configuration?
+              add_terraform_backend_source(local_definition_folder)
+            end
+
+            the_instance
           end
         end
 
@@ -61,6 +68,20 @@ module Cloudspin
           else
             raise UnsupportedStackDefinitionLocationError, @definition_location
           end
+        end
+
+        def add_terraform_backend_source(terraform_source_folder)
+
+          puts "Creating file #{terraform_source_folder}/_cloudspin_created_backend.tf"
+
+          File.open("#{terraform_source_folder}/_cloudspin_created_backend.tf", 'w') { |backend_file|
+            backend_file.write(<<~TF_BACKEND_SOURCE
+              terraform {
+                backend "s3" {}
+              }
+            TF_BACKEND_SOURCE
+            )
+          }
         end
 
         def the_usual_configuration_files
@@ -117,6 +138,10 @@ module Cloudspin
             puts instance.init_dry
             puts instance.down_dry
             puts instance.down
+          end
+
+          task :refresh do
+            puts instance.refresh
           end
         end
 
